c655d393ca4a9454c2ef38b2c4b6983eeda40231
Theharishpatel <harishpatel0964@gmail.com>
2026-03-11 23:25:07 +0530

    feat: auth api create for register and login routes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ const app = express()
 
 app.use(express.json())
 
+/* require all the routes here */
+const authRounter = require("./routes/auth.routes")
 
+// using all the routes here
+app.use("/api/auth", authRounter)
 
 module.exports = app
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,6 +2,24 @@ const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
     username:{
-        type
+       type: String,
+       unique: [true, "username already taken"],
+       require: true, 
+    },
+    
+    email: {
+        type: String,
+        unique:[true, "Account already exists with this email address"],
+        require : true,
+    },
+
+    password: {
+        type: String,
+        require: true,
     }
+   
 })
+
+const userModel = mongoose.model("users", userSchema)
+
+module.exports= userModel
